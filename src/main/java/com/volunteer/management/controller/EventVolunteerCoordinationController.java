88daa1c6b4f0ef297ee2b41e6594133b5e3ba264
mani <mannikantachunduru@gmail.com>
2023-07-19 11:16:41 -0500

    send mail Implementation

--- src/main/java/com/volunteer/management/controller/EventVolunteerCoordinationController.java
+++ src/main/java/com/volunteer/management/controller/EventVolunteerCoordinationController.java
@@ -7,6 +7,7 @@ import com.volunteer.management.entity.Volunteer;
 
 import com.volunteer.management.dto.EventVolunteerMatchDto;
 import com.volunteer.management.service.EventVolunteerCoordinationService;
+import com.volunteer.management.service.MailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -25,6 +27,9 @@ public class EventVolunteerCoordinationController {
 
     @Autowired
     ResponseDto responseDto;
+
+    @Autowired
+    MailSenderService mailSenderService;
 
     @PostMapping("/sortVolunteers")
     public ModelAndView getVolunteerByDateOfEventCreation(@ModelAttribute("matchDto") EventVolunteerMatchDto eventVolunteerMatchDto, Model model){
@@ -37,9 +42,36 @@ public class EventVolunteerCoordinationController {
             model.addAttribute("status", responseDto);
 
         }else{
-            responsePage = "VolunteersList";
-            model.addAttribute("listOfVolunteers",fetchedVolunteers);
+            if(eventVolunteerMatchDto.getSortVolunteersbyDate().equals("after")){
+                responsePage = "VolunteersList";
+                model.addAttribute("listOfVolunteers", fetchedVolunteers);
+            }else if(eventVolunteerMatchDto.getSortVolunteersbyDate().equals("before")){
+                responsePage = "VolunteerListBefore";
+                model.addAttribute("listOfVolunteers", fetchedVolunteers);
+            }
         }
         return new ModelAndView(responsePage);
     }
+
+//    @PostMapping("/sendMail")
+//    public void sendMailToVolunteer(@ModelAttribute("volunteerDto") VolunteerDto volunteerDto, Model model){
+//        System.out.println("Execution is here");
+//        System.out.println(volunteerDto.getVolunteerId());
+//    }
+
+
+    @PostMapping("/sendMail")
+    public void sendMailToVolunteer(@ModelAttribute("volunteerDto") VolunteerDto volunteerDto, Model model, HttpServletRequest request) {
+        String[] selectedVolunteers = request.getParameterValues("selectedVolunteers");
+
+        if (selectedVolunteers != null) {
+            for (String volunteerEmailAddress : selectedVolunteers) {
+                volunteerDto.setEmailAddress(volunteerEmailAddress);
+                String volunteerEmail = volunteerDto.getEmailAddress();
+                mailSenderService.sendSimpleEmail( volunteerEmail,"volunteer Availability Check","Hello Volunteer! \n are you interested in joining the event Camp");
+
+            }
+        }
+    }
+
 }
